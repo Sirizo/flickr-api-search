88b68ee054b39ea749c6a0d9e30901f27f005004
Etienne Donzelot <etiennedonzelot@Etiennes-iMac.fritz.box>
2018-07-16 07:26:03 +0100

    Added files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # flickr-api-search
+
+Searching the public Flickr API feed via tags. Defaults to the 'sea' tag at the start.
---
+++ scripts/logic.js
@@ -0,0 +1,100 @@
+function results(){
+     searchFlickr('sea');
+     $('#searchBtn').click(function (){
+          $('.album .row').html('');
+          if ($('#search').val() == null || $('#search').val() == '') {
+               searchFlickr('sea');
+          } else {
+               searchFlickr($('#search').val().toLowerCase().replace(" ",""));
+          }
+     });
+}
+
+function searchFlickr(searchTerm) {
+
+     $.ajax({
+          url: 'https://api.flickr.com/services/feeds/photos_public.gne?tags='+searchTerm+'&tagmode=all&safe_search=1&format=json', 
+          type: 'GET',
+          dataType: "jsonp",
+          jsonpCallback: 'jsonFlickrFeed',
+          jsonp: "jsonp",
+          success: function( msg ) {
+               // Make feedData equal to the data within the 'items' outer object of msg
+               var feedData = msg.items ? msg.items : {};
+
+               var title, img, author, lnk, authlnk, proflnk, description, tags;
+
+               // Go through feedData and print out each entry provided it has something
+               for (var i = 0; i < 8; i++) {
+
+                    if(typeof feedData[i].title !== 'undefined'){
+
+                         title = feedData[i].title;
+                         img = feedData[i].media.m;
+                         author = getAuthor(feedData[i].author);
+                         lnk = feedData[i].link;
+                         authlnk = getLinks(feedData[i].link);
+                         description = getDescription(feedData[i].description);
+                         tags = getTags(feedData[i].tags);
+
+                         // Output everything
+                         $('.album .row').append('<div class="col-md-3">\
+                                                    <div class="card mb-4 box-shadow">\
+                                                      <a class="imageCard" href="'+lnk+'">\
+                                                       <img class="card-img-top" alt="" src="'+img+'" data-holder-rendered="true">\
+                                                      </a>\
+                                                      <div class="card-body">\
+                                                        <div class="d-flex justify-content-between align-items-center">\
+                                                          <p><a href="'+lnk+'">'+title+'</a> by <a href="'+authlnk+'">'+author+'</a></p>\
+                                                        </div>\
+                                                        <p class="card-text">'+description+'</p>\
+                                                        <div class="d-flex justify-content-between align-items-center">\
+                                                          <p>Tags: <a href="https://www.flickr.com/photos/tags/'+tags[0]+'">'+tags[0]+'</a>, <a href="https://www.flickr.com/photos/tags/'+tags[1]+'">'+tags[1]+'</a>, <a href="https://www.flickr.com/photos/tags/'+tags[2]+'">'+tags[2]+'</a>, <a href="'+lnk+'">...</a></p>\
+                                                        </div>\
+                                                      </div>\
+                                                    </div>\
+                                                  </div>')
+
+                    }
+               }
+          }
+     });
+}
+
+function getAuthor(tmp1) {
+     var tmp2, author;
+     tmp2 = tmp1.replace('nobody@flickr.com ("', '');
+     author = tmp2.replace('")', '');
+
+     return author;
+}
+
+function getDescription(description) {
+     // Sanitize desriptions
+     if ($.parseHTML(description).length == 5) {
+          description = '';
+          // console.log('There is no description for this entry');
+     } else if ($.parseHTML(description).length == 6) {
+          if ($.parseHTML(description)[5].innerHTML.length > 100) {
+               return description = $.parseHTML(description)[5].textContent.substring(0,100) + '...';
+          } else {
+               return description = $.parseHTML(description)[5].textContent;
+          }
+     }
+}
+
+function getLinks(profile) {
+     var tmp, tmplnk, proflnk;
+
+     tmp = profile.slice(0, -2);
+     tmplnk = tmp.split("\/");     
+     tmplnk.pop();    
+     
+     return proflnk = tmplnk.join("\/");
+}
+
+function getTags(tags) {
+     return tags.split(' ').slice(0,3);
+}
+
+results();
